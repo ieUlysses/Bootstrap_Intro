6e02a8e1fe36efd2abdc48fe8c7864d4fb03fc4e
James Conway <71008025+ieUlysses@users.noreply.github.com>
2022-01-27 10:34:23 +0100

    v1 for About page complete  v2 will be drier

--- src/Componants/AboutImg/AboutImg.jsx
+++ src/Componants/AboutImg/AboutImg.jsx
@@ -4,7 +4,7 @@ import aboutImg from "../../Media/aboutImg.JPG"
 
 function AboutImg() {
     return (<>
-        <div style={{ height: "100%" }} className='container w-75 d-flex align-items-center '><img src={aboutImg} class="img-fluid" alt="A smiling image of James Conway"></img></div>
+        <div style={{ height: "100%" }} className='container w-75 d-flex align-items-center '></div>
     </>)
 }
 
--- src/Componants/AboutText/AboutText.jsx
+++ src/Componants/AboutText/AboutText.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 function AboutText() {
     return (<>
 
-        <p className='text-start'>
 
+        <p style={{ fontFamily: "Roboto" }} className='text-start'>
             Ich bin der James. 23 Jahre alt aus Irland.
-            Im oktober letztes jahr habe ich mein studium zum  Fullstack web developer bei der DCI Berlin erfolgereich abgeschloseen.
-            dieses jahr suche ich nach ein passesnden stelle als junior Frontend Developer.
-
-
-
         </p>
+        <p style={{ fontFamily: "Roboto" }} >
+            Im oktober letztes jahr habe ich mein studium zum  Fullstack web developer bei der DCI Berlin erfolgereich abgeschloseen.
+        </p>
+        <p style={{ fontFamily: "Roboto" }} >
+            dieses jahr suche ich nach ein passesnden stelle als junior Frontend Developer.
+        </p>
+
+
+
+
+
 
     </>)
 }
