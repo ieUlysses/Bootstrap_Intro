1e7d70a0e09078af2f3467427186f76239aaa914
James Conway <71008025+ieUlysses@users.noreply.github.com>
2022-01-27 15:09:32 +0100

    saving progress: Need to finish up motivation cards then look into color mode

---
+++ src/Componants/MotivationHeader/MotivationHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MotivationHeader() {
+    return (<>
+
+        <div className='container-fluid bg-dark d-flex justify-content-center'>
+            <p className='display-2 text-warning' style={{ fontFamily: "Roboto" }}>
+                Why should I be your new <strong style={{ fontFamily: "YellowTail" }}>Frontend-Developer</strong>?
+            </p>
+
+
+        </div>
+    </>
+    )
+}
+
+export default MotivationHeader;
--- src/Pages/Motivation.jsx
+++ src/Pages/Motivation.jsx
@@ -1,11 +1,41 @@
 import React from "react"
-import ContactForm from "../Componants/ContactForm/ContactForm"
+import MotivationCards from "../Componants/MotivationCards/MotivationCards"
+import MotivationHeader from "../Componants/MotivationHeader/MotivationHeader"
+
 
 
 function Motivation() {
 
     return (
         <>
+
+
+
+
+
+            <div style={{ height: "100%" }} className="row ">
+
+                <div className="col-sm-8 d-flex align-items-center" style={{ backgroundColor: "black" }}  >
+                    <MotivationHeader />
+                </div>
+
+                <div className="col-sm-4  d-flex flex-column justify-content-evenly align-items-center  ">
+
+                </div>
+
+
+            </div>
+
+            <div style={{ height: "100%" }} className="row ">
+
+                <div className="col-sm-12 d-flex align-items-center" style={{ backgroundColor: "black" }}  >
+                    <MotivationCards />
+                </div>
+
+            </div>
+
+
+
 
 
         </>
