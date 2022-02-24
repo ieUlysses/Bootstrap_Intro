f443e764a2a54fbf7659740118ba5ee32526c498
James Conway <71008025+ieUlysses@users.noreply.github.com>
2022-02-24 13:05:07 +0100

    added cards + modal for projects

--- src/Componants/TechStackText/TechStackText.jsx
+++ src/Componants/TechStackText/TechStackText.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function TechStackText() {
     return (<>
-        <p>
+        <p style={{ fontFamily: "Roboto" }}>
             During my time with the DCI I concentrated on the MERN stack, building fullstack applications. paying particular attention to their frontend aperance, and using a mix of Vanilla css or libraries such as MUI depending on the project/  brief.
         </p>
-        <p>
+        <p style={{ fontFamily: "Roboto" }}>
             Following Graduation i have been gradually honing these skills and keeping up to date with updates, staying inline with best practices.
         </p>
     </>)
